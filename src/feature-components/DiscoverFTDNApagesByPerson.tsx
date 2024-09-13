2f8e0231c98b244f6c71ecd8d4675247a748477a
Andrii Lundiak <landike@gmail.com>
2024-09-13 15:28:59 +0200

    Small changes

--- src/feature-components/DiscoverFTDNApagesByPerson.tsx
+++ src/feature-components/DiscoverFTDNApagesByPerson.tsx
@@ -4,13 +4,13 @@ export function DiscoverFTDNApagesByPerson() {
     <section>
       <ul>
         <li>
-          <a href="https://discover.familytreedna.com/y-dna/I-FGC63213/tree">Andrii Lundiak</a>
-        </li>
-        <li>
           <a href="https://discover.familytreedna.com/y-dna/I-FT278687/tree">Taras Kravchenko</a>
         </li>
         <li>
           <a href="https://discover.familytreedna.com/y-dna/I-BY78168/story">Vasyl Popovych (MRCA: Rev. Joachimi Lewicki)</a>
+        </li>
+        <li>
+          <a href="https://discover.familytreedna.com/y-dna/I-FTF87323/tree">Ivan Zenevych (level of Krakauer 8)</a>
         </li>
       </ul>
     </section>
