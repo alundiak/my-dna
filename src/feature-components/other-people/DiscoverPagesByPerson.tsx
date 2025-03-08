b4d535ed20a61de43716e79c7037c8fc9e4be339
Andrii Lundiak <landike@gmail.com>
2025-03-08 14:29:24 +0100

    A few changes

--- src/feature-components/other-people/DiscoverPagesByPerson.tsx
+++ src/feature-components/other-people/DiscoverPagesByPerson.tsx
@@ -9,25 +9,31 @@ export function DiscoverPagesByPerson() {
         <section>
           <ul>
             <li>
-              <a href="https://discover.familytreedna.com/y-dna/I-FT278687/tree">I2a-Y3118+ - Taras Kravchenko</a>
+              <a href="https://discover.familytreedna.com/y-dna/I-FT278687">I2a-Y3118+ - Taras Kravchenko</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/y-dna/I-BY78168/story">I2a-BY78168 - Vasyl Popovych (MRCA: Rev. Joachimi Lewicki)</a>
+              <a href="https://discover.familytreedna.com/y-dna/I-BY78168">I2a-BY78168 - Vasyl Popovych (MRCA: Rev. Joachimi Lewicki)</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/y-dna/I-FTF87323/tree">I2a-FTF87323 Ivan Zenevych (level of Krakauer 8)</a>
+              <a href="https://discover.familytreedna.com/y-dna/I-FTF87323">I2a-FTF87323 Ivan Zenevych (level of Krakauer 8)</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/y-dna/I-BY37221/tree">I2a-BY37221 - Mr from Lugansk (and also Rivne, Polatava and Mr Rajkov from Macedonia) - all nearby Zarubino/Irkutsk 32 ancient sample</a>
+              <a href="https://discover.familytreedna.com/y-dna/I-BY37221">I2a-BY37221 - Mr from Lugansk (and also Rivne, Polatava and Mr Rajkov from Macedonia) - all nearby Zarubino/Irkutsk 32 ancient sample</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/y-dna/N-M6921/tree">N1c-M6921 - Igor Dychta (Ukraine, Poland, Lithuania ... Gediminids )</a>
+              <a href="https://discover.familytreedna.com/y-dna/N-M6921">N1c-M6921 - Igor Dychta (Ukraine, Poland, Lithuania ... Gediminids )</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/y-dna/R-BY50762/tree">R1b-U152\BY50762 - Vasyl Josypenko</a>
+              <a href="https://discover.familytreedna.com/y-dna/R-BY20951">R1b-BY20951 - Dmitriy Romanyuk</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/y-dna/I-Z63/tree">I1-Z63 - Сергій Фазульянов</a>
+              <a href="https://discover.familytreedna.com/y-dna/R-BY50762">R1b-U152\BY50762 - Vasyl Josypenko</a>
+            </li>
+            <li>
+              <a href="https://discover.familytreedna.com/y-dna/R-Y2905">R-Y2905 - Ігор Розкладай</a>
+            </li>
+            <li>
+              <a href="https://discover.familytreedna.com/y-dna/I-Z63">I1-Z63 - Сергій Фазульянов</a>
             </li>
           </ul>
         </section>
@@ -35,13 +41,19 @@ export function DiscoverPagesByPerson() {
         <section>
           <ul>
             <li>
-              <a href="https://discover.familytreedna.com/mtdna/J1b1a1b/tree">J1b1a1b - Jaroslav Sumyshyn</a>
+              <a href="https://discover.familytreedna.com/mtdna/J1b1a1b">J1b1a1b - Jaroslav Sumyshyn</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/mtdna/I-FT278687/tree">TBD - Ігор Розкладай</a>
+              <a href="https://discover.familytreedna.com/mtdna/TBD">TBD - Ігор Розкладай</a>
             </li>
             <li>
-              <a href="https://discover.familytreedna.com/mtdna/U5b2a1a/tree">U5b2a1a-T16311C! - Сергій Фазульянов</a>
+              <a href="https://discover.familytreedna.com/mtdna/U5b2a1a">U5b2a1a-T16311C! - Сергій Фазульянов</a>
+            </li>
+            <li>
+              <a href="https://discover.familytreedna.com/mtdna/H1b2">мама Dmitriy Romanyuk</a>
+            </li>
+            <li>
+              <a href="https://discover.familytreedna.com/mtdna/W3a1/">Sebastian Magnowski(Jezupol)</a>
             </li>
           </ul>
         </section>
