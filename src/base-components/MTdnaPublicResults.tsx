30fb3b6b07acdcb900856d7cfc96c3d327a37b61
Andrii Lundiak <landike@gmail.com>
2025-06-03 02:29:43 +0200

    Added types

--- src/base-components/MTdnaPublicResults.tsx
+++ src/base-components/MTdnaPublicResults.tsx
@@ -1,28 +1,41 @@
-import { FTDNA_PUBLIC } from "../shared/constant";
-import { KeyIcon } from "./icons/KeyIcon";
+import { FTDNA_PUBLIC } from '../shared/constant';
+import { KeyIcon } from './icons/KeyIcon';
+
+const mapper = (item: any, index: number) => {
+  if (item.mtDnaResults === false) {
+    return null;
+  }
+
+  const key = `${index}-${item.idGroup}-mt-dna-results`;
+
+  const mtDNAResultsURL = `${FTDNA_PUBLIC}/${item.idPublic}?iframe=mtresults`;
+  const mtMapResultsURL = `${FTDNA_PUBLIC}/${item.idPublic}?iframe=mtmap`;
+
+  return (
+    <li key={key}>
+      <span>{item.idPublic}</span>
+      {' | '}
+      <a href={mtDNAResultsURL} target="_blank">
+        mtResults
+      </a>
+      {' | '}
+      <a href={mtMapResultsURL} target="_blank">
+        mtMap
+      </a>
+      {item.resultsRequireLogin && (
+        <>
+          {' | '}
+          <KeyIcon />
+        </>
+      )}
+    </li>
+  );
+};
 
 export function MTdnaPublicResults({ data }: { data: any }) {
-  return <>
-    <ul>
-      {
-        data.map((item: any, index: number) => {
-
-          if (item.mtDnaResults === false) {
-            return null;
-          }
-
-          const key = `${index}-${item.idGroup}-mt-dna-results`;
-
-          const mtDNAResultsURL = `${FTDNA_PUBLIC}/${item.idPublic}?iframe=mtresults`;
-          const mtMapResultsURL = `${FTDNA_PUBLIC}/${item.idPublic}?iframe=mtmap`;
-
-          return (
-            <li key={key}>
-              <span>{item.idGroup}</span> | <a href={mtDNAResultsURL} target="_blank">mtResults</a> | <a href={mtMapResultsURL} target="_blank">mtMap</a> {item.resultsRequireLogin && (<>| <KeyIcon /></>)}
-            </li>
-          );
-        })
-      }
-    </ul>
-  </>;
+  return (
+    <>
+      <ul>{data.map(mapper)}</ul>
+    </>
+  );
 }
--- src/feature-components/y-dna/YdnaPagesBySNP.tsx
+++ src/feature-components/y-dna/YdnaPagesBySNP.tsx
@@ -4,51 +4,58 @@ import { parseSNP_data } from '../../shared/common';
 import { FTDNA_DISCOVER_Y_DNA, SNP_TRACKER_URL, THEY_TREE_URL, YFULL_URL } from '../../shared/constant';
 import { YdnaSNPmatchPeople } from './YdnaSNPmatchPeople';
 
+interface MyCustomData {
+  snp: string;
+  people: object;
+  sameLevelSNPs?: string[];
+  children?: MyCustomData;
+}
+
+const renderListItems = (element: MyCustomData | string, index: number) => {
+  const { SNP, people } = parseSNP_data(element);
+  const key = `${index}-${SNP}-y-dna-snp`;
+  const ftdnaURL = `${FTDNA_DISCOVER_Y_DNA}/${SNP}`; // TBD: maybe add/extend with suffixes
+  const yfullURL = `${YFULL_URL}/${SNP}`;
+  const snpTrackerURL = `${SNP_TRACKER_URL}?snp=${SNP}`;
+  const theyTreeURL = `${THEY_TREE_URL}?snp=${SNP}`;
+
+  const elements = [
+    <span>{SNP}</span>,
+    <a href={ftdnaURL} target="_blank">
+      FTDNA Discover
+    </a>,
+    <a href={yfullURL} target="_blank">
+      YFULL
+    </a>,
+    <a href={snpTrackerURL} target="_blank">
+      SNP Tracker
+    </a>,
+    <a href={theyTreeURL} target="_blank">
+      TheyTree
+    </a>,
+  ];
+
+  return (
+    <li key={`list-item-${key}`}>
+      {elements.flatMap((el, i) => {
+        if (i > 0) {
+          return [' | ', <span key={`sep-${i}`}>{el}</span>];
+        } else {
+          return [<span key={`sep-${i}`}>{el}</span>];
+        }
+      })}
+      {people && <YdnaSNPmatchPeople names={people} />}
+    </li>
+  );
+};
+
 export function YdnaPagesBySNP() {
-  const mapper = (element: any, index: number) => {
-    const { SNP, people } = parseSNP_data(element);
-    const key = `${index}-${SNP}-y-dna-snp`;
-    const ftdnaURL = `${FTDNA_DISCOVER_Y_DNA}/${SNP}`; // TBD: maybe add/extend with suffixes
-    const yfullURL = `${YFULL_URL}/${SNP}`;
-    const snpTrackerURL = `${SNP_TRACKER_URL}?snp=${SNP}`;
-    const theyTreeURL = `${THEY_TREE_URL}?snp=${SNP}`;
-
-    const elements = [
-      <span>{SNP}</span>,
-      <a href={ftdnaURL} target="_blank">
-        FTDNA Discover
-      </a>,
-      <a href={yfullURL} target="_blank">
-        YFULL
-      </a>,
-      <a href={snpTrackerURL} target="_blank">
-        SNP Tracker
-      </a>,
-      <a href={theyTreeURL} target="_blank">
-        TheyTree
-      </a>,
-    ];
-
-    return (
-      <li key={`list-item-${key}`}>
-        {elements.flatMap((el, i) => {
-          if (i > 0) {
-            return [' | ', <span key={`sep-${i}`}>{el}</span>];
-          } else {
-            return [<span key={`sep-${i}`}>{el}</span>];
-          }
-        })}
-        {people && <YdnaSNPmatchPeople names={people} />}
-      </li>
-    );
-  };
-
   return (
     <>
       <h3 id="y-dna-pages-by-snp">Y-DNA pages by SNP</h3>
       <Container className="bg-light border">
         <section>
-          <ul>{i2aSNPdata.map(mapper)}</ul>
+          <ul>{(i2aSNPdata as MyCustomData[] | string[]).map(renderListItems)}</ul>
         </section>
       </Container>
     </>
