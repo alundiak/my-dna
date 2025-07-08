c3f41986d81b18166471b49fb4a76725cc277752
Andrii Lundiak <landike@gmail.com>
2025-07-08 20:35:56 +0200

    Experimental change to optimize bundle size of JavaScript files

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,48 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   build: {
+    minify: true,
     outDir: 'build', // default value = 'dist
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (
+              id.includes('bootstrap') ||
+              id.includes('reactstrap') ||
+              id.includes('@popperjs/core')
+            ) {
+              return 'bootstrap';
+            }
+
+            if (
+              id.includes('prop-types') ||
+              id.includes('react') ||
+              id.includes('react-') ||
+              id.includes('scheduler')
+            ) {
+              return 'react';
+            }
+
+            return 'other-vendors';
+          }
+
+          return undefined;
+        },
+      },
+    },
   },
   base: '/my-dna',
+  server: {
+    watch: {
+      ignored: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/build/**',
+        '**/.git/**',
+        '**/*.log',
+        '**/temp/**',
+      ],
+    },
+  },
 });
